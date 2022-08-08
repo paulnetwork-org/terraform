289c7ba1cd7b0b180d25dbffec3e11523f5c3a06
pauldualsim <33259209+pauldualsim@users.noreply.github.com>
2022-08-09 00:58:49 +0300

    Create cluster.tf

---
+++ azure/cluster.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "laroa" {
+  name     = "laroa"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "laroa" {
+  name                = "laroa"
+  location            = azurerm_resource_group.laroa.location
+  resource_group_name = azurerm_resource_group.laroa.name
+  dns_prefix          = "laroa" 
+
+  default_node_pool {
+    name           = "nodes"
+    node_count     = 2
+    vm_size        = "Standard_D2_v2"
+    
+  }
+
+  linux_profile {
+    admin_username = "ubuntu"
+    ssh_key {
+      key_data = var.ssh_key
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
