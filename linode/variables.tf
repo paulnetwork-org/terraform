539a02955a35a28454d54c61d9792efd4ff5a6f2
pauldualsim <33259209+pauldualsim@users.noreply.github.com>
2022-08-12 14:40:18 +0300

    Update variables.tf

--- linode/variables.tf
+++ linode/variables.tf
@@ -4,43 +4,14 @@ variable "token" {
     sensitive = true
 }
 
-variable "k8s_version" {
-    description = "1.23"
+variable "ssh_key" {
+    default = "~/.ssh/id_rsa.pub"
 }
 
-variable "label" {
-    description = "laroa-cluster"
+variable "admin_password" {
+    description = "your admin username password"
 }
 
-variable "region" {
-    description = "eu-central"
+variable "admin_username" {
+  default = "ubuntu"
 }
-
-variable "tags" {
-    description = "Tags to apply to your cluster"
-    type = list(string)
-    default = ["website"]
-}
-
-variable "pools" {
-    description = "laroa"
-    type = list(object({
-        type = string
-        count = number
-      }))
-    default = [
-        {
-          type = "g6-standard-4"
-          count = 3
-        }
-      ]
-    }
-
-variable "ssh_public_key" {
-    default = "/home/node/.ssh/id_rsa.pub"
-}
-
-variable "root_password" {
-    default = "Terraform123"
-}
-
