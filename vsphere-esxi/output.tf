8d7b0cbb28ec21c4037ab32f20d0d14fe25f2104
pauldualsim <33259209+pauldualsim@users.noreply.github.com>
2022-09-17 12:14:16 +0300

    Update output.tf

--- vsphere-esxi/output.tf
+++ vsphere-esxi/output.tf
@@ -1,3 +1,7 @@
 output "control_ip_addresses" {
  value = vsphere_virtual_machine.control.*.default_ip_address
 }
+
+output "worker_ip_addresses" {
+ value = vsphere_virtual_machine.worker.*.default_ip_address
+}
