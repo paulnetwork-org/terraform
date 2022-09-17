246288dfb55c345a5282b216e6170868ce0afc8c
pauldualsim <33259209+pauldualsim@users.noreply.github.com>
2022-09-17 12:14:32 +0300

    Update main.tf

--- vsphere-esxi/main.tf
+++ vsphere-esxi/main.tf
@@ -125,10 +125,4 @@ resource "vsphere_virtual_machine" "worker" {
     }
 }
 
-output "control_ip_addresses" {
- value = vsphere_virtual_machine.control.*.default_ip_address
-}
 
-output "worker_ip_addresses" {
- value = vsphere_virtual_machine.worker.*.default_ip_address
-}
