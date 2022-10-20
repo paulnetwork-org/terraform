eb87ec99b07314c21afc63e2c16441b5ea0ffc4d
Adrian Paul <33259209+pauldualsim@users.noreply.github.com>
2022-10-20 19:15:00 +0300

    Create variables.tf

---
+++ aws/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+# Create a EC2 Instance (Ubuntu 22.04)
+variable "ami" {
+    type = string
+    default = "ami-08c40ec9ead489470"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+#variable "access_ip" {
+#  type = string
+#}
+
