677fdc2318289655aab7169fa2edd99e87dad720
Adrian Paul <33259209+pauldualsim@users.noreply.github.com>
2022-12-13 11:02:46 +0200

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -11,11 +11,41 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_pet" "paul" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "paul" {
+  name     = "paul"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "paul" {
+  name                = "paul"
+  location            = azurerm_resource_group.paul.location
+  resource_group_name = azurerm_resource_group.paul.name
+  dns_prefix          = "paul" 
+
+  default_node_pool {
+    name           = "nodes"
+    node_count     = 2
+    vm_size        = "Standard_D2_v3"
+    
+  }
+
+  linux_profile {
+    admin_username = "ubuntu"
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
 module "kubernetes_deployment" {
   source = "./modules/website/"
 }
-
-#module "mysql" {
-#  source = "./modules/mysql/"
-#}
 
