961ab4a1b7c8b6667b6f4051eafb7d9309b0a2cc
Adrian Paul Vamos <33259209+pauldualsim@users.noreply.github.com>
2022-12-21 13:08:11 +0200

    Create variables.tf

---
+++ azure/fileshare/variables.tf
@@ -0,0 +1,16 @@
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
+#variable "location" {
+#  default = "westeurope"
+#}
+
