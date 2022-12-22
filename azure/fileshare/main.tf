30afeca591ad7bffb98ac179c55d2d81208c4d63
Adrian Paul Vamos <33259209+pauldualsim@users.noreply.github.com>
2022-12-22 11:51:55 +0200

    Update main.tf

--- azure/fileshare/main.tf
+++ azure/fileshare/main.tf
@@ -31,11 +31,11 @@ resource "azurerm_kubernetes_cluster" "test2" {
     name           = "nodes"
     node_count     = 2
     vm_size        = "Standard_D2_v3"
-       
+    os_sku         = "Ubuntu"
   }
 
-  linux_profile {
-    admin_username = "ubuntu"
+    linux_profile {
+    admin_username = "azure_ubuntu"
     ssh_key {
       key_data = file(var.ssh_public_key)
     }
@@ -46,8 +46,74 @@ resource "azurerm_kubernetes_cluster" "test2" {
   }
 }
 
-
-module "kubernetes_fileshare" {
-  source = "./modules/fileshare/"
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.test2.kube_config.0.client_certificate
+  sensitive = true
 }
 
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.test2.kube_config_raw
+
+  sensitive = true
+}
+
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.test2.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.test2.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.test2.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.test2.kube_config.0.cluster_ca_certificate)
+}
+
+
+resource "kubernetes_persistent_volume_claim" "azurefile" {
+  metadata {
+    name = "azurefile"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    storage_class_name = "azurefile"
+    resources {
+      requests = {
+        storage = "5Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name = "nginx-deployment"
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:latest"
+          volume_mount {
+            name      = "azurefile"
+            mount_path = "/mnt/azure"
+          }
+        }
+        volume {
+          name = "azurefile"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.azurefile.metadata.0.name
+          }
+        }
+      }
+    }
+  }
+}
