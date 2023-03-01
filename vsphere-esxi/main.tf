dcdfd2b1bb1e4561bd9223b9d809e99478302a0d
Adrian Paul Vamos <33259209+pauldualsim@users.noreply.github.com>
2023-03-02 00:17:49 +0200

    Update main.tf

--- vsphere-esxi/main.tf
+++ vsphere-esxi/main.tf
@@ -1,128 +1,92 @@
-# main.tf
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.3.1"
+    }
+  }
+}
 
-# Configure the vSphere provider
 provider "vsphere" {
-    user = var.vsphereuser
-    password = var.vspherepass
-    vsphere_server = var.vsphere-vcenter
-    allow_unverified_ssl = var.vsphere-unverified-ssl
+  user           = var.vsphere-user
+  password       = var.vsphere-password
+  vsphere_server = var.vsphere-vcenter
+  
+  # If you have a self-signed cert
+  allow_unverified_ssl = var.vsphere-unverified-ssl
 }
 
+# Define VMware vSphere 
 data "vsphere_datacenter" "dc" {
-    name = var.vsphere-datacenter
+  name = var.vsphere-datacenter
 }
+
+#data "vsphere_host" "host" {
+#  name          = var.esxi-name
+#  datacenter_id = data.vsphere_datacenter.dc.id
+#}
 
 data "vsphere_datastore" "datastore" {
-    name = var.vm-datastore
-    datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_compute_cluster" "cluster" {
-    name = var.vsphere-cluster
-    datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_network" "network" {
-    name = var.vm-network
-    datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-data "vsphere_virtual_machine" "template" {
-    name = var.vm-template-name
-    datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Create VM Folder
-resource "vsphere_folder" "folder" {
-  path          = "k8s"
-  type          = "vm"
+  name          = var.vm-datastore
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Create Control VMs
-resource "vsphere_virtual_machine" "control" {
-    count = var.control-count
-    name = "${var.vm-prefix}-controller-${count.index + 1}"
-    resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-    datastore_id = data.vsphere_datastore.datastore.id
-    folder = vsphere_folder.folder.path
-    
-    num_cpus = var.vm-cpu
-    memory = var.vm-ram
-    guest_id = var.vm-guest-id
-
-    
-    network_interface {
-        network_id = data.vsphere_network.network.id
-    }
-
-    disk {
-        label = "${var.vm-prefix}-${count.index + 1}-disk"
-        size  = 25
-    }
-
-    clone {
-        template_uuid = data.vsphere_virtual_machine.template.id
-        customize {
-            timeout = 0
-            
-            linux_options {
-            host_name = "${var.vm-prefix}-controller-${count.index + 1}"
-            domain = var.vm-domain
-            }
-            
-            network_interface {
-            ipv4_address = "10.10.10.10${count.index + 1}"
-            ipv4_netmask = 24
-            }
-
-            ipv4_gateway = "10.110.10.1"
-            dns_server_list = [ "10.10.10.2" ]
-
-        }
-    }
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere-cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Create Worker VMs
-resource "vsphere_virtual_machine" "worker" {
-    count = var.worker-count
-    name = "${var.vm-prefix}-worker-${count.index + 1}"
-    resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-    datastore_id = data.vsphere_datastore.datastore.id
-    folder = vsphere_folder.folder.path
-    
-    num_cpus = var.vm-cpu
-    memory = var.vm-ram
-    guest_id = var.vm-guest-id
-    
-    network_interface {
-        network_id = data.vsphere_network.network.id
-    }
-
-    disk {
-        label = "${var.vm-prefix}-${count.index + 1}-disk"
-        size  = 25
-    }
-
-    clone {
-        template_uuid = data.vsphere_virtual_machine.template.id
-        customize {
-            timeout = 0
-            
-            linux_options {
-            host_name = "${var.vm-prefix}-worker-${count.index + 1}"
-            domain = var.vm-domain
-            }
-            
-            network_interface {
-            ipv4_address = "10.10.10.11${count.index + 1}"
-            ipv4_netmask = 24
-            }
-
-            ipv4_gateway = "10.10.10.1"
-            dns_server_list = [ "10.10.10.2" ]
-        }
-    }
+data "vsphere_network" "network" {
+  count = length(var.vm-names)
+  name = var.vm-network[count.index]
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+data "vsphere_virtual_machine" "template" {
+  name          = "/${var.vsphere-datacenter}/vm/${var.vsphere-template-folder}/${var.vm-template-name}"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Create VMs
+resource "vsphere_virtual_machine" "vm" {
+  firmware = "efi"
+  count = length(var.vm-names)
+  name  = var.vm-names[count.index]
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id  = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vm-cpu[count.index]
+  memory   = var.vm-ram[count.index]
+  guest_id = var.vm-guest-id
+  
+
+  network_interface {
+    network_id = data.vsphere_network.network[count.index].id
+    adapter_type = var.vm-adapter_type
+  }
+
+  disk {
+    label = var.vm-names[count.index]
+    thin_provisioned = true
+    size  = var.vm_disk_size_gb[count.index]
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = var.vm-names[count.index]
+        domain = var.vm-domain
+      }     
+      network_interface {
+        ipv4_address  = var.vm_ipv4_addresses[count.index]
+        ipv4_netmask  = var.vm_ipv4_netmask  
+      }
+      ipv4_gateway  = var.vm_ipv4_gateways[count.index]
+      timeout = 30
+    }
+  }
+}
+
 
 
