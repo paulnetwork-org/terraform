e447bc1c2d029d6677f8afde7d22768d461e1b51
Adrian Paul Vamos <33259209+pauldualsim@users.noreply.github.com>
2023-03-02 00:18:05 +0200

    Update output.tf

--- vsphere-esxi/output.tf
+++ vsphere-esxi/output.tf
@@ -1,7 +1,8 @@
-output "control_ip_addresses" {
- value = vsphere_virtual_machine.control.*.default_ip_address
-}
-
-output "worker_ip_addresses" {
- value = vsphere_virtual_machine.worker.*.default_ip_address
+output "vm_name_to_ipv4_address" {
+  value = [
+    for i in range(length(var.vm-names)) : {
+      name      = var.vm-names[i]
+      ipv4_addr = var.vm_ipv4_addresses[i]
+    }
+  ]
 }
