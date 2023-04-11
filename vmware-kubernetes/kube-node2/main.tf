a5fca86499431976f8775a178578ec5332c24c63
Adrian Paul Vamos <33259209+paulnetwork-org@users.noreply.github.com>
2023-04-11 21:23:23 +0300

    Create main.tf

---
+++ vmware-kubernetes/kube-node2/main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.3.1"
+    }
+  }
+}
+
+
+# Define VMware vSphere 
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere-datacenter
+}
+
+data "vsphere_host" "host" {
+  name          = var.esxi
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vm-datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+#data "vsphere_compute_cluster" "cluster" {
+#  name          = var.vsphere-cluster
+#  datacenter_id = data.vsphere_datacenter.dc.id
+#}
+
+data "vsphere_network" "network" {
+  name = var.vm-network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "/${var.vsphere-datacenter}/vm/${var.vsphere-template-folder}/${var.vm-template-name}"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Create VMs
+resource "vsphere_virtual_machine" "vm" {
+  firmware = "efi"
+  name  = var.vm-names
+  resource_pool_id = data.vsphere_host.host.resource_pool_id
+  datastore_id  = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vm-cpu
+  memory   = var.vm-ram
+  guest_id = var.vm-guest-id
+  
+  
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+    adapter_type = var.vm-adapter_type
+  }
+
+  disk {
+    label = var.vm-names
+    thin_provisioned = true
+    size  = var.vm_disk_size_gb
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = var.vm-names
+        domain = var.vm-domain
+      }     
+      network_interface {
+        ipv4_address  = var.vm_ipv4_address
+        ipv4_netmask  = var.vm_ipv4_netmask  
+      }
+      ipv4_gateway  = var.vm_ipv4_gateway
+      timeout = 30
+    }
+  }
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    password = var.linux_pass
+    host     = vsphere_virtual_machine.vm.default_ip_address
+  }
+    provisioner "remote-exec" {
+      inline = [
+        "sleep 5",
+        "sudo ${file("join.txt")}"
+      ]
+    }
+
+}
+
+
