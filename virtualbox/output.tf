208cad730da76657b0e71ccadc6fb4ba1ab8d375
Adrian Paul Vamos <33259209+paulnetwork-org@users.noreply.github.com>
2024-02-01 18:05:01 +0200

    Delete virtualbox directory

--- virtualbox/output.tf
+++
@@ -1,9 +0,0 @@
-output "kubemasterip1" {
-  value = virtualbox_vm.masternode.*.network_adapter.0.ipv4_address
-}
-output "workerip1" {
-  value = element(virtualbox_vm.workernode.*.network_adapter.0.ipv4_address, 1)
-}
-output "workerip2" {
-  value = element(virtualbox_vm.workernode.*.network_adapter.0.ipv4_address, 2)
-}
